ad5a943a39b752e57fb666278d0690c04a3c7492
Anthony <a.sabbagh@era2140.com>
2022-03-17 22:47:21 +0100

    feat: add cli

---
+++ bin/index.js
@@ -0,0 +1,25 @@
+const inquirer = require('inquirer');
+inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'collection',
+            message: 'What collection would you modify ?',
+            choices: ['article', 'user', 'article']
+        },
+        {
+            name: 'data',
+            message: 'Write your data'
+        }
+    ])
+    .then(({ collection, data }) => {
+        try {
+            JSON.parse(data)
+            console.log(`${collection} modified with data ${data}`)
+        } catch (e) {
+            throw new Error(e)
+        }
+    })
+    .catch((e) => {
+        console.error('an error occured')
+    })

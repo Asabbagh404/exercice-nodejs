3c65220e6d14a0dfc3eec0e94fd8d871dfea8730
Anthony <a.sabbagh@era2140.com>
2022-03-17 22:55:40 +0100

    feat: add commander

--- bin/index.js
+++ bin/index.js
@@ -1,25 +1,32 @@
-const inquirer = require('inquirer');
-inquirer
-    .prompt([
-        {
-            type: 'list',
-            name: 'collection',
-            message: 'What collection would you modify ?',
-            choices: ['article', 'user', 'article']
-        },
-        {
-            name: 'data',
-            message: 'Write your data'
-        }
-    ])
-    .then(({ collection, data }) => {
-        try {
-            JSON.parse(data)
-            console.log(`${collection} modified with data ${data}`)
-        } catch (e) {
-            throw new Error(e)
-        }
+#!/usr/bin/env node
+const { program } = require('commander');
+const chalk = require('chalk');
+
+program
+    .version(`test/cli ${require('../package').version}`)
+    .usage('<command> [options]')
+
+program
+    .command('create')
+    .description('create an document in database')
+    .action((name, options) => {
+        require('../lib/createData')(name, options)
     })
-    .catch((e) => {
-        console.error('an error occured')
-    })
+
+// output help information on unknown commands
+program.on('command:*', ([cmd]) => {
+    program.outputHelp()
+    console.log(`  ` + chalk.red(`Unknown command ${chalk.yellow(cmd)}.`))
+    process.exitCode = 1
+})
+
+// add some useful info on help
+program.on('--help', () => {
+    console.log()
+    console.log(`  Run ${chalk.cyan(`era <command> --help`)} for detailed usage of given command.`)
+    console.log()
+})
+
+program.parse(process.argv)
+
+program.commands.forEach(c => c.on('--help', () => console.log()))
